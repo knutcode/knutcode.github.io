14f6b679c338cc317aa8afed9713b5ff5700b1a3
knutc <knutc.kodehode@gmail.com>
2022-08-16 14:38:33 +0200

    added links to projects page & modified/index.css

--- src/pages/LandingPage/Projects.js
+++ src/pages/LandingPage/Projects.js
@@ -1,7 +1,61 @@
 const Projects = () => {
   return (
     <main>
-      <h1 className="highlight">projects</h1>
+      <h2 className="underline">kodehode assignments</h2>
+      <ul className="project-list">
+        <li>
+          <a href="https://knutcode.github.io/kodehode/todo-assignment/" target="_blank" rel="noreferrer">
+            to-do list, @Kodehode
+          </a>
+        </li>
+        <li>
+          <a href="https://knutcode.github.io/kodehode/pokemon-api/" target="_blank" rel="noreferrer">
+            pokemon generator, @Kodehode
+          </a>
+        </li>
+        <li>
+          <a href="https://knutcode.github.io/kodehode/collatz-conjencture/" target="_blank" rel="noreferrer">
+            collatz calculator, @Kodehode
+          </a>
+        </li>
+        <li>
+          <a href="https://knutcode.github.io/kodehode/drumkit-assignment/" target="_blank" rel="noreferrer">
+            drumkit, @Kodehode
+          </a>
+        </li>
+      </ul>
+
+      <h2 className="underline">scrimba projects</h2>
+      <ul className="project-list">
+        <li>
+          <a href="https://knutcode.github.io/scrimba/invoice-creator/" target="_blank" rel="noreferrer">
+            invoice app, @Scrimba
+          </a>
+        </li>
+        <li>
+          <a href="https://knutcode.github.io/scrimba/password-generator/" target="_blank" rel="noreferrer">
+            password generator, @Scrimba
+          </a>
+        </li>
+        <li>
+          <a href="https://knutcode.github.io/scrimba/unit-converter/" target="_blank" rel="noreferrer">
+            unit converter, @Scrimba
+          </a>
+        </li>
+        <li>
+          <a href="https://knutcode.github.io/scrimba/bbq-splash-page/" target="_blank" rel="noreferrer">
+            bbq splash page, @Scrimba
+          </a>
+        </li>
+      </ul>
+      <h2 className="underline">personal projects</h2>
+      <ul className="project-list">
+        <li>
+          <a href="https://knutcode.github.io/is-it-friday/" target="_blank" rel="noreferrer">
+            is it friday?
+          </a>
+        </li>
+      </ul>
     </main>
   );
 };

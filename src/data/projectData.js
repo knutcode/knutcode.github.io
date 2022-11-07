ac97c63d32aaf87652314465a28202e1b3e86ebe
knutc <knutc.kodehode@gmail.com>
2022-11-07 14:59:34 +0100

    replaced catfacts from projects with watchlist

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,17 +1,19 @@
 const data = [
 	{
 		id: 1,
-		title: 'Cat Facts',
-		desc: 'Small app that shows you random facts from the catfact.ninja API. Made in React using Axios and styled-components.',
-		github: 'https://github.com/knutcode/cat-facts',
-		pages: 'https://knutcode.github.io/cat-facts/',
-		image: require('../img/CatFacts.png'),
-		alt: 'Cat facts',
+		title: 'Movie Watchlist',
+		desc:
+			'Find and save movies locally to your watchlist using the OMDb API, Made in React using Axios, useContext, useReducer and Tailwind.css',
+		github: 'https://github.com/knutcode/movie-watchlist',
+		pages: 'https://knutcode.github.io/movie-watchlist/',
+		image: require('../img/MovieWatchlist.png'),
+		alt: 'Movie Watchlist',
 	},
 	{
 		id: 2,
 		title: 'To-do List',
-		desc: 'To-do list to keep track of your daily dasks, local-storage saves your list locally. Made with JavaScript, HTML and CSS.',
+		desc:
+			'To-do list to keep track of your daily dasks, local-storage saves your list locally. Made with JavaScript, HTML and CSS.',
 		github: 'https://github.com/knutcode/kodehode/tree/main/todo-assignment',
 		pages: 'https://knutcode.github.io/kodehode/todo-assignment/',
 		image: require('../img/TodoList.png'),
